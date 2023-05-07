02f429aa7bb2bab945eefbf0f5d7f69c690cc099
Sageer-M-Kabeer <sageerm379@gmail.com>
2023-05-07 23:26:45 +0000

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <div className="footer">
+         <p>&copy;2023 Sageer's foodary </p>
+      </div>
+    );
+  }
+  export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,40 @@
+export const my_recipe = {
+    recipe_1: {
+      author:'saheer',
+      image:'/img/recipe.jpg',
+      name: 'Chirashi Don',
+      preparation_time: '30 minutes',
+      servings: '2',
+      ingredients: [
+        '1/2 cup rice',
+        '1/2 cup water',
+        '2 eggs',
+        '300g of salmon, tuna and salmon roe',
+      ],
+      instructions: [
+        'Cook rice and water in a rice cooker',
+        'Fry eggs in a pan',
+        'Serve rice, eggs and fish on a plate',
+      ],
+   
+    },
+
+    recipe_2: {
+      image:'/img/recipe2.jpg',
+      name: 'Test Dson',
+      preparation_time: '37 minutes',
+      servings: '2',
+      ingredients: [
+        '1/2 cup rice',
+        '1/2 cup water',
+        '2 eggs',
+        '300g of salmon, tuna and salmon roe',
+      ],
+      instructions: [
+        'Cook rice and water in a rice cooker',
+        'Fry eggs in a pan',
+        'Serve rice, eggs and fish on a plate',
+      ],
+   
+    },
+  }
